6d43544b8ebb9d02ba02718aae724dc36ac6a680
frkhovshar <fkhoushab@msn.com>
2025-11-03 22:03:13 -0800

    Delete app.py

--- app.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.title("Monopoly Simulation Test")
-st.write("Hello Fram — your Streamlit app is connected perfectly 🚀")
-st.success("If you can see this message, everything is working!")
